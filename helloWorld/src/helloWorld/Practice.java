54925a8626d76a31dc9f03aa48ce8357fe6456a8
BT100 <BT100@MEACBT209-10a.ac.sdmesa.net>
2024-03-14 19:01:38 -0700

    initial commit

---
+++ helloWorld/src/helloWorld/Practice.java
@@ -0,0 +1,9 @@
+package helloWorld;
+
+public class Practice {
+
+	public static void main(String[]args) {
+		System.out.println("Hello World");
+		System.out.println(" I will try to push this project to github.com ");
+	}
+}
